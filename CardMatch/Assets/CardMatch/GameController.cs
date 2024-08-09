46bb66e59b71e64c643dcd5ffb21d0345a9d7115
MAINPC\John Titor <theoralb@gmail.com>
2024-08-09 22:28:51 +0700

    GameController Design

---
+++ CardMatch/Assets/CardMatch/GameController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //Init Game-State Coroutine Here
+    }
+
+    IEnumerator GameStateController()
+    {
+        // Draw UI
+
+        // Prepare Card Table
+
+        // Count-Down  3 - 2 - 1 GO!
+
+        // Wait Playing State
+
+        yield return null;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
